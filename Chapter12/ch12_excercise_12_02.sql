b6e4438de129006b3b12cd181e3bb31633d8f217
Daniël van Eeden <git@myname.nl>
2020-04-01 21:47:31 +0200

    Update chapter12

--- Chapter12/ch12_excercise_12_02.sql
+++ Chapter12/ch12_excercise_12_02.sql
@@ -1,5 +1,6 @@
 --disable_warnings
 DROP USER IF EXISTS 'sarah'@'%';
+DROP USER IF EXISTS 'webserver2'@'%';
 --enable_warnings
 
 CREATE USER 'sarah'@'%' IDENTIFIED BY 'IkbyewUgJeuj8';
@@ -10,4 +11,14 @@ GRANT ALL ON mobileapp.* TO 'mike'@'%';
 GRANT ALL ON mobileapp.* TO 'patrick'@'%';
 GRANT ALL ON mobileapp.* TO 'webserver'@'%';
 
+REVOKE ALL ON ebike.* FROM 'mike'@'%';
+
 ALTER USER 'patrick'@'%' IDENTIFIED BY 'WimgeudJa';
+
+# SHOW GRANTS FOR 'webserver'@'%';
+
+CREATE USER 'webserver2'@'%' IDENTIFIED BY 'dutPyicloHi' WITH MAX_USER_CONNECTIONS 300;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `ebike`.* TO `webserver2`@`%`;
+GRANT ALL PRIVILEGES ON `mobileapp`.* TO `webserver2`@`%`;
+
+ALTER USER 'webserver'@'%' ACCOUNT LOCK;
