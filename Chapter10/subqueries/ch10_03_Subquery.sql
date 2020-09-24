8b3f0011cc01a8e9ce1a9a4a9ffb8c27b348b196
Szabo <dszabo@itron.com>
2020-09-24 15:28:30 +0200

    Finalized chapter 10 codebase

--- Chapter10/subqueries/ch10_03_Subquery.sql
+++ Chapter10/subqueries/ch10_03_Subquery.sql
@@ -4,8 +4,4 @@ SELECT Name FROM city WHERE CountryCode=(
   SELECT Code FROM country WHERE Name='Romania'
 );
 
-SELECT
-  Name,
-  CountryCode,
-  (SELECT Name FROM country WHERE Code=city.CountryCode) AS CountryName
-FROM city;
+
