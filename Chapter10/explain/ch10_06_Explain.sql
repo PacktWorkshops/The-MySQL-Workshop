cb3e4763f1789db240ca577d96beb1845c1a77fa
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-10-08 12:34:03 +0530

    Update ch10_06_Explain.sql

--- Chapter10/explain/ch10_06_Explain.sql
+++ Chapter10/explain/ch10_06_Explain.sql
@@ -7,24 +7,20 @@ ALTER TABLE city ADD PRIMARY KEY (ID);
 EXPLAIN SELECT * FROM city WHERE ID=2460;
 
 EXPLAIN SELECT * FROM country co LEFT JOIN city ci
-ON ci.CountryCode=co.Code WHERE ci.ID=540;
+ON ci.CountryCode=co.Code WHERE ci.ID=540\G
 
 ALTER TABLE country ADD PRIMARY KEY (Code);
 
 EXPLAIN SELECT * FROM country co LEFT JOIN city ci
-ON ci.CountryCode=co.Code WHERE ci.ID=540;
+ON ci.CountryCode=co.Code WHERE ci.ID=540\G
 
 EXPLAIN FORMAT=JSON SELECT * FROM country co
-LEFT JOIN city ci ON ci.CountryCode=co.Code WHERE ci.ID=540;
+LEFT JOIN city ci ON ci.CountryCode=co.Code WHERE ci.ID=540\G
 
 EXPLAIN FORMAT=TREE SELECT * FROM country co 
 LEFT JOIN city ci ON ci.CountryCode=co.Code
-WHERE ci.ID=540;
+WHERE ci.ID=540\G
 
-# Requires MySQL 8.0.18 or newer
-# Replace column 1 as it has timing info that differs
-# per run.
---replace_column 1 OUTPUT
 EXPLAIN ANALYZE SELECT * FROM country co 
 LEFT JOIN city ci ON ci.CountryCode=co.Code
-WHERE ci.ID=540;
+WHERE ci.ID=540\G
