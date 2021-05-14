542298a384a549d849c6c5a29d0d48cf7ed81277
scprogramming <scottc130@gmail.com>
2021-05-13 22:10:45 -0400

    Updating file name

--- Chapter05/Exercise5.06/TestModule.js
+++ Chapter05/Exercise5.06/TestModule.js
@@ -1,6 +1,6 @@
 var mysqlconnection = require("./mysqlconnection.js");
 
-mysqlconnection.query("SELECT Count(*) AS CountryCount FROM	import_test.country;", function (err, SQLresult) {
+mysqlconnection.query("SELECT Count(*) AS CountryCount FROM	backuppractice.country;", function (err, SQLresult) {
 
 if (err) throw "Problem counting Countries:- " + err.code;
 console.log("Country count :- " + SQLresult[0].CountryCount);
