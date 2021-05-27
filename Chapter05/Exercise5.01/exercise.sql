af1d948ba0a2233e168cb0c22ae41f6876da6a65
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2021-05-27 12:26:07 +0530

    Update and rename Exercise 5_4.sql to exercise.sql

---
+++ Chapter05/Exercise5.01/exercise.sql
@@ -0,0 +1,10 @@
+SELECT * FROM backuppractice.country;
+
+SELECT * FROM backuppractice.country 
+  WHERE `Country Code`="AUS"
+
+DELETE FROM backuppractice.country 
+  WHERE `Country Code`="AUS";
+
+SELECT * FROM backuppractice.country 
+  WHERE `Country Code`="AUS";
