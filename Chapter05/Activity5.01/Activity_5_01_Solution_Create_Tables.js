c98d5a3b3c365cf9466e18c44291efa60554c840
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2021-05-27 16:43:39 +0530

    Update Activity_5_01_Solution_Create_Tables.js

--- Chapter05/Activity5.01/Activity_5_01_Solution_Create_Tables.js
+++ Chapter05/Activity5.01/Activity_5_01_Solution_Create_Tables.js
@@ -1,28 +1,26 @@
-
-
 var mysqlconnection = require("./mysqlconnection.js");
-		var sql = "CREATE TABLE `MOTdatabase`.`Customers` (\
-		`CustID` int(11) NOT NULL AUTO_INCREMENT, \
-		`CustomerName` varchar(50) NOT NULL, \
-		PRIMARY KEY (`CustID`)\
-		);"
-		
-		 mysqlconnection.query(sql, function (err) {
-      		if (err) throw "Problem creating the Table:- " + err.code;	
-      		console.log("Table created"); 	
+var sql = "CREATE TABLE `MOTdatabase`.`Customers` (
+  `CustID` int(11) NOT NULL AUTO_INCREMENT, 
+  `CustomerName` varchar(50) NOT NULL, 
+  PRIMARY KEY (`CustID`)
+);"
+
+mysqlconnection.query(sql, function (err) {
+  if (err) throw "Problem creating the Table:- " + err.code;
+  console.log("Table created");
 });
 
-		var sql = "CREATE TABLE `MOTdatabase`.`CustomerPurchases` (\
-		`CPID` int(11) NOT NULL AUTO_INCREMENT, \
-		`CustID` int(11) NOT NULL, \
-		`SKU` varchar(20) NOT NULL, \
-		`SaleDateTime` varchar(25) NOT NULL, \
-		`Quantity` int(11) NOT NULL, \
-		PRIMARY KEY (`CPID`)\
-		);"
-		
-		 mysqlconnection.query(sql, function (err) {
-      		if (err) throw "Problem creating the Table:- " + err.code;	
-      		console.log("Table created"); 	
-			process.exit();
+var sql = "CREATE TABLE `MOTdatabase`.`CustomerPurchases` (
+  `CPID` int(11) NOT NULL AUTO_INCREMENT, 
+  `CustID` int(11) NOT NULL, 
+  `SKU` varchar(20) NOT NULL, 
+  `SaleDateTime` varchar(25) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  PRIMARY KEY (`CPID`)
+);"
+
+mysqlconnection.query(sql, function (err) {
+  if (err) throw "Problem creating the Table:- " + err.code;
+  console.log("Table created"); 	
+  process.exit();
 });
